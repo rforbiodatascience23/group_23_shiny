3ecc78aa2b6d2c391c755798645517f9ce2ce802
LFT18 <s222797@student.dtu.dk>
2023-10-31 11:21:47 +0100

    Add functions

--- R/mod_script.R
+++ R/mod_script.R
@@ -26,6 +26,42 @@ mod_script_ui <- function(id){
 mod_script_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
+    dna <- reactiveVal()
+
+    output$DNA <- renderUI({
+      textAreaInput(
+        inputId = ns("DNA"),
+        label = "DNA sequence",
+        placeholder = "Insert DNA sequence",
+        value = dna(),
+        height = 100,
+        width = 600
+      )
+
+
+      observeEvent(input$generate_dna, {
+        dna(
+          enigma::generate_DNA_seq(input$seq_len)
+        )
+      })
+
+      output$aminoacid_seq <- renderText({
+        # Ensure input is not NULL and is longer than 2 characters
+        if(is.null(input$DNA_seq)){
+          NULL
+        } else if(nchar(input$DNA_seq) < 3){
+          NULL
+        } else{
+          input$DNA_seq |>
+            toupper() |>
+            enigma::transcribe() |>
+            enigma::extracted_codons() |>
+            enigma::codon_to_aa()
+        }
+      })
+
+    })
+
 
   })
 }
